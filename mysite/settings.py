58018748d898d5c367d23df982fe63647a4d7577
Ga Min Cha <gmcha0323@khu.ac.kr>
2025-10-13 21:50:38 +0900

    Edit ALLOWED_HOSTS in /mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-aa4ui=bk#m7b@*7^8l@n=ast$u$t#%3#53+x!cy9lfh#l*o+rg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'pythonanywhere.com']
 
 
 # Application definition
